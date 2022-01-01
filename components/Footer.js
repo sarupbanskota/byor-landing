6aeaae27731a8f63bb5e7a2b4381b512d36c6153
Mohammed Zeeshan <work.zeeshan@gmail.com>
2022-01-01 19:54:39 +0530

    empty footer for now

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,5 @@
-export {Footer}
+export { Footer };
 
 function Footer() {
-  return (
-    <footer className="">
-      <div className="">
-        <div className="">
-          <p>
-            <strong>Footer</strong>
-          </p>
-        </div>
-      </div>
-    </footer>
-  )
+  return <footer></footer>;
 }
