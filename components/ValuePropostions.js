da6bf13fcbc3c69e542ff2e477973354c7651a92
Sarup Banskota <sbanskota08@gmail.com>
2022-02-01 16:48:21 +0800

    Update is this for me

--- components/ValuePropostions.js
+++ components/ValuePropostions.js
@@ -9,36 +9,36 @@ const values = [
   },
   {
     id: 2,
-    desc: "You're curious about how your favorite technologies you use, work internally.",
+    desc: "You're curious how your favorite technologies work internally.",
   },
   {
     id: 3,
-    desc: "You're looking for a self-paced, minimal guidance approach (vs hand-holding).",
+    desc: "You're a self-paced, minimal guidance person (vs requiring hand-holding).",
   },
 ];
 
 function ValuePropositions() {
   return (
-    <section className="relative bg-blue-50 pt-16 pb-32 overflow-hidden">
+    <section className="relative bg-radial-at-b from-black to-gray-900 py-16 overflow-hidden">
       <div className="relative">
         <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
           <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
             <div>
               <div className="mt-6">
-                <h2 className="text-center md:text-left text-2xl md:text-3xl font-extrabold tracking-tight text-gray-900">
+                <h2 className="text-center md:text-left text-2xl md:text-3xl font-extrabold tracking-tight text-white">
                   Is this course for me?
                 </h2>
                 <div className="mt-10 space-y-4 md:space-y-6 lg:space-y-10 max-w-lg">
                   {values.map((item) => (
                     <div key={item.id} className="relative">
-                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md  text-white">
+                      <div className="absolute flex items-center justify-center h-12 w-12 rounded-md text-white">
                         <img
                           className="rounded-full w-6 md:w-12 h-10 md:h-10"
                           src="/for/icons/check.svg"
                           alt=""
                         />
                       </div>
-                      <p className="ml-16 md:text-2xl leading-6 font-medium text-gray-900">
+                      <p className="ml-16 md:text-2xl leading-6 font-medium text-white">
                         {item.desc}
                       </p>
                     </div>
@@ -51,9 +51,9 @@ function ValuePropositions() {
           <div className="mt-12 sm:mt-16 lg:mt-0 -mr-2.5 md:mr-0">
             <div className="pl-4 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <img
-                className="w-full lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                src="app-screenshot-2.png"
-                alt="Inbox user interface"
+                className="w-full lg:absolute lg:-right-[220px] lg:h-full lg:w-auto lg:max-w-none"
+                src="/for/redis-course-light.png"
+                alt="Redis Course Screenshot"
               />
             </div>
           </div>
