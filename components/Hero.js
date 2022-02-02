30acc88c6e89ee668b46dae390cd789b3a77628c
Sarup Banskota <sbanskota08@gmail.com>
2022-02-03 01:04:49 +0800

    Pass props into Hero

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import { LogoCloud } from "./LogoCloud";
 import { PrimaryButton } from "./PrimaryButton";
 export { Hero };
 
-function Hero() {
+function Hero({title, subtitle, testimonial}) {
   return (
     <div className="bg-radial-at-b from-teal-400 via-lime-50 to-white">
       <div className="relative overflow-hidden">
@@ -11,11 +11,10 @@ function Hero() {
           <div className="mt-8 mx-auto max-w-7xl px-4 sm:mt-8 sm:px-6">
             <div className="text-center">
               <h1 className="text-4xl tracking-tight font-extrabold text-transparent bg-clip-text bg-black sm:text-5xl md:text-6xl">
-                The Rust Pro’s <br /> Best Kept Secret.
+                {title}
               </h1>
               <p className="mt-3 max-w-md mx-auto text-lg sm:text-xl md:mt-5 md:text-2xl md:max-w-3xl">
-                Build a real project. Achieve depth. <br />
-                Become your team’s Rust expert.
+                {subtitle}
               </p>
               {/* <div className="mt-8">
                 <PrimaryButton text="Choose Project →" />
@@ -24,17 +23,9 @@ function Hero() {
                 </span>
               </div> */}
               <p className="italic text-lg max-w-xl mt-8 inline-block">
-                “My favorite way to master a language.” <br />
+                {testimonial.quote}<br />
                 <span className="text-sm">
-                  — Pranjal Paliwal, $35k winner of{" "}
-                  <a
-                    href="https://hackatom.org/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="underline underline-offset-4"
-                  >
-                    HackAtom 2021
-                  </a>
+                  {testimonial.author}
                 </span>
               </p>
             </div>
--- pages/rust.js
+++ pages/rust.js
@@ -17,7 +17,22 @@ export default function Home() {
         <link rel="icon" href="/for/favicon.ico" />
       </Head>
       <main className="w-full bg-radial-at-b from-teal-100 via-lime-50 to-white">
-        <Hero />
+        <Hero title={<>The Rust Pro’s <br /> Best Kept Secret.</>}
+         subtitle={<>Build a real project. Achieve depth. <br />
+          Become your team’s Rust expert.</>
+        }
+        testimonial={{
+          quote: <>“My favorite way to master a language.”</>,
+          author: <>— Pranjal Paliwal, $35k winner of{" "}
+          <a
+            href="https://hackatom.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="underline underline-offset-4"
+          >
+            HackAtom 2021
+          </a></>
+        }}/>
         <FeatureCard />
         <Testimonial />
         <ValuePropositions />
