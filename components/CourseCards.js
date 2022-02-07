a3f7c79757ca55a7a2f6215a4cd9b4e6e9240009
Mohammed Zeeshan <work.zeeshan@gmail.com>
2022-02-07 18:09:53 +0530

    fix: course-card-logo-issue, use min-w-max instead

--- components/CourseCards.js
+++ components/CourseCards.js
@@ -144,14 +144,14 @@ function CourseCards({ title, waitlist, upcomingOnly }) {
             Vote for an upcoming course, and get automatic early access
           </p>
         )}
-        <div className="my-8 md:my-0 course-cards flex space-x-8 no-scrollbar overflow-x-scroll items-stretch py-10">
+        <div className="my-8 md:my-0 course-cards flex space-x-8 no-scrollbar overflow-x-scroll items-center py-10">
           {courses
             .filter((c) => (upcomingOnly ? c.upcoming : !c.upcoming))
             .map((course) => (
               <a
                 href={`https://app.codecrafters.io/courses/${course.key}`}
                 key={course.key}
-                className="flex align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left shadow-lg hover:shadow-xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full sm:p-6 !pr-0 justify-between items-stretch min-w-fit border border-gray-100"
+                className="flex align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left shadow-lg hover:shadow-xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full sm:p-6 !pr-0 justify-between items-center min-w-max border border-gray-100"
               >
                 <div>
                   {" "}
