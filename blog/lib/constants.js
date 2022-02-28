a1a86676d1ec7d78892732289f61cad6dd397470
Sarup Banskota <sbanskota08@gmail.com>
2022-02-28 15:24:45 -0800

    png

--- blog/lib/constants.js
+++ blog/lib/constants.js
@@ -1,5 +1,5 @@
 export const EXAMPLE_PATH = 'cms-sanity'
 export const CMS_NAME = 'Sanity'
 export const CMS_URL = 'https://sanity.io/'
-export const HOME_OG_IMAGE_URL = 'https://codecrafters.io/blog/og-image'
+export const HOME_OG_IMAGE_URL = 'https://codecrafters.io/blog/og-image.png'
   
