72bc4e4a7dcda1b10bee4d6a0417c0ab2e461759
Sarup Banskota <sbanskota08@gmail.com>
2022-02-28 14:23:25 -0800

    try basepath

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ allPosts, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>CodeCrafters Blog</title>
         </Head>
         <Container>
           <Intro />
